f996b8c51db60e7fa2842f2a5cc04ccb1c6e7560
Sofia Kondirova <Sofia.Kondirova@jetbrains.com>
2024-12-04 23:31:17 +0100

    little change

--- src/main/java/org/example/SimpleJavaClass.java
+++ src/main/java/org/example/SimpleJavaClass.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class SimpleJavaClass   {
     public void simple(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello from SimpleClass");
     }
 }
